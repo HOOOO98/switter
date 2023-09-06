75c44248f3fef9b65c018741914a6ce4c98be485
HOOOO98 <loading2us@gmail.com>
2023-09-06 09:02:59 +0900

    Delete sweet func

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { dbService } from "../fBase";
+import { dbService, storageService } from "../fBase";
 
 const Sweet = ({ sweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
@@ -8,6 +8,7 @@ const Sweet = ({ sweetObj, isOwner }) => {
     const ok = window.confirm("Are you sure you want to delete this sweet?");
     if (ok) {
       await dbService.doc(`sweets/${sweetObj.id}`).delete();
+      await storageService.refFromURL(sweetObj.attachmentUrl).delete();
     }
   };
   const toggleEditing = async () => setEditing((prev) => !prev);
@@ -38,6 +39,7 @@ const Sweet = ({ sweetObj, isOwner }) => {
         ) : (
           <>
             <h4>{sweetObj.text}</h4>
+            {sweetObj.attachmentUrl && <img src={sweetObj.attachmentUrl} width="50px" height="50px" alt=""/>}
             {isOwner && (
               <>
                 <button onClick={onDeleteClick}>Delete Sweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "../fBase";
+import { dbService, storageService } from "../fBase";
+import { v4 as uuidv4 } from "uuid";
 import Sweet from "../components/Sweet";
 
 const Home = ({ userObj }) => {
   const [sweet, setSweet] = useState("");
   const [sweets, setSweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   useEffect(() => {
     dbService.collection("sweets").onSnapshot(snapshot => {
       const sweetArray = snapshot.docs.map(doc => ({
@@ -17,12 +19,21 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("sweets").add({
+    let attachmentUrl = "";
+    if (attachment !== undefined) {
+      const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const sweetObj = {
       text: sweet,
       createAt: Date.now(),
       creatorId: userObj.uid,
-    });
+      attachmentUrl,
+    }
+    await dbService.collection("sweets").add(sweetObj);
     setSweet("");
+    setAttachment("");
   };
 
   const onChange = (event) => {
@@ -38,10 +49,14 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
     }
     reader.readAsDataURL(theFile);
-  }
+  };
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -49,6 +64,11 @@ const Home = ({ userObj }) => {
         <input value={sweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Sweet" />
+        {attachment &&
+          <div>
+            <img src={attachment} width="50px" height="50px" alt="" />
+            <button onClick={onClearAttachment}>Cancel upload</button>
+          </div>}
       </form>
       <div>
         {sweets.map((sweet) => (
